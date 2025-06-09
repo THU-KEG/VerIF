3b1df981494a31b51f496e59329c2a117ef40b72
h-peng17 <penghao20170136@163.com>
2025-06-09 14:29:32 +0800

    update path to model

--- examples/grpo_trainer/run_qwen2-7b_verif.sh
+++ examples/grpo_trainer/run_qwen2-7b_verif.sh
@@ -17,7 +17,7 @@ python3 -m verl.trainer.main_ppo \
     data.max_response_length=4096 \
     data.filter_overlong_prompts=True \
     data.truncation='error' \
-    actor_rollout_ref.model.path=/mnt/ph/MODELS/DeepSeek-R1-Distill-Qwen-7B \
+    actor_rollout_ref.model.path=DeepSeek-R1-Distill-Qwen-7B \
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.model.use_remove_padding=True \
     actor_rollout_ref.actor.ppo_mini_batch_size=32 \
--- examples/grpo_trainer/run_tulu3-8b_verif.sh
+++ examples/grpo_trainer/run_tulu3-8b_verif.sh
@@ -17,7 +17,7 @@ python3 -m verl.trainer.main_ppo \
     data.max_response_length=4096 \
     data.filter_overlong_prompts=True \
     data.truncation='error' \
-    actor_rollout_ref.model.path=/mnt/ph/MODELS/Llama-3.1-Tulu-3-8B-SFT \
+    actor_rollout_ref.model.path=Llama-3.1-Tulu-3-8B-SFT \
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.model.use_remove_padding=True \
     actor_rollout_ref.actor.ppo_mini_batch_size=32 \
